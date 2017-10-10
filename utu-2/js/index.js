e3fc7add2ebbae60694b598429ab0424d40a46df
葱花思鸡蛋 <514164510@qq.com>
2017-10-10 16:23:15 +0800

    网页界面搭建完成

---
+++ utu-2/js/index.js
@@ -0,0 +1,17 @@
+/**
+ * Created by home on 2017/9/27.
+ */
+function play() {
+    // alert("hello")
+    console.log("hello word");
+};
+
+function getSrcImg()
+{
+    console.log("hello word");
+};
+
+function ac() {
+    // alert("hello")
+    console.log("hello word");
+};
